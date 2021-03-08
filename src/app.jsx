f41cf17e1c4deab24d3f608b5a5863f258c07c10
Yong Hyuk Seo <seoyongs@sfu.ca>
2021-03-07 19:02:12 -0800

    add project

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
-import './app.css';
+import React, { useEffect, useState } from "react";
+import styles from "./app.module.css";
+import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
-function App() {
-  return <h1>Hello :)</h1>;
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+  }, [youtube]);
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
